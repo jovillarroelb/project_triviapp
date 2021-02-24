ce3b808ce6203a7c38bc767cf3626bed17e74618
Jorge Villarroel <51089372+RLEjvillarroel@users.noreply.github.com>
2021-02-24 17:01:41 -0300

    Upload project Trivia_API

--- backend/models.py
+++ backend/models.py
@@ -3,9 +3,8 @@ from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-# Database definition
-database_name = "trivia"
-database_path = "postgres://{}:{}@{}/{}".format('postgres', 'postgres','localhost:5432', database_name)
+database_name = "trivia_dev"
+database_path = "postgres://{}/{}".format('localhost:5432', database_name)
 
 db = SQLAlchemy()
 
