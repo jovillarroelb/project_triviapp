a9fa7534b53b01dfdf27d7af0d971b4e8b71e142
Jorge Villarroel <jovillarroelb@gmail.com>
2021-03-11 01:57:54 -0300

    Update.

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -10,21 +10,46 @@ from models import setup_db, Question, Category
 class TriviaTestCase(unittest.TestCase):
     """This class represents the trivia test case"""
 
+    # # Original - Jorge Villarroel
+    # def setUp(self):
+    #     """Define test variables and initialize app."""
+    #     self.app = create_app()
+    #     self.client = self.app.test_client
+    #     self.database_name = "trivia_test"
+    #     self.database_path = "postgres://{}/{}".format(
+    #         'localhost:5432', self.database_name)
+    #     setup_db(self.app, self.database_path)
+
+    #     # binds the app to the current context
+    #     with self.app.app_context():
+    #         self.db = SQLAlchemy()
+    #         self.db.init_app(self.app)
+    #         # create all tables
+    #         self.db.create_all()
+
+    # Suggested by Udacity's project reviewer
     def setUp(self):
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
-        self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format(
-            'localhost:5432', self.database_name)
-        setup_db(self.app, self.database_path)
+        self.DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')
+        self.DB_USER = os.getenv('DB_USER', 'postgres')
+        self.DB_PASSWORD = os.getenv('DB_PASSWORD', 'postgres')
+        self.DB_NAME = os.getenv('DB_NAME', 'trivia_test')
+        self.DB_PATH = 'postgresql+psycopg2://{}:{}@{}/{}'.format(
+            self.DB_USER,
+            self.DB_PASSWORD,
+            self.DB_HOST,
+            self.DB_NAME)
+        
+        setup_db(self.app, self.DB_PATH)
 
         # binds the app to the current context
         with self.app.app_context():
             self.db = SQLAlchemy()
             self.db.init_app(self.app)
             # create all tables
-            self.db.create_all()
+            self.db.cr
 
     def tearDown(self):
         """Executed after reach test"""
